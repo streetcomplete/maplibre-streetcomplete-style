c96081a38f9e96e82ee79a69238d674083d21cc3
Tobias Zwick <newton@westnordost.de>
2024-08-13 19:31:03 +0200

    use only Roboto as font
    for simplicity (only need to download one font)

--- Main.kt
+++ Main.kt
@@ -99,7 +99,7 @@ fun createStyle(name: String, accessToken: String, languages: List<String>, colo
     val defaultTextStyle = Text(
         text = coalesceName(),
         size = byZoom(1.0 to 13.0, 24.0 to 64.0),
-        fonts = listOf("Roboto Regular", "Noto Regular"),
+        fonts = listOf("Roboto Regular"),
         color = colors.text,
         outlineColor = colors.textOutline,
         outlineWidth = 2.5,
@@ -436,7 +436,7 @@ fun createStyle(name: String, accessToken: String, languages: List<String>, colo
         Layer("labels-country",
             src = "place_label",
             filter = listOf(tagIs("class", "country")),
-            paint = defaultTextStyle.copy(fonts = listOf("Roboto Bold", "Noto Bold"))
+            paint = defaultTextStyle.copy(fonts = listOf("Roboto Bold"))
         ),
 
         Layer("labels-localities",
